4a5cc42be7eed4e3f1293f50259ce8a2dfaed0e4
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-04-26 16:11:59 +0200

    New script / JAM

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomn : MonoBehaviour
+{
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void Explode()
+    {
+        //Bomb explose apres un certain temps
+    }
+}
