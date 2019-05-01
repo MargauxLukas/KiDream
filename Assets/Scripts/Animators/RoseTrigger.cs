68a4c6c953d1df6a0526f50460176b4a576496a7
LUKAS Margaux <m.lukas@rubika-edu.com>
2019-05-01 23:18:31 +0200

    Creation Scene Level01

---
+++ Assets/Scripts/Animators/RoseTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoseTrigger : MonoBehaviour
+{
+    Animator roseAnim;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            roseAnim.SetBool("isTouched", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            roseAnim.SetBool("isTouched", false);
+        }
+    }
+}
