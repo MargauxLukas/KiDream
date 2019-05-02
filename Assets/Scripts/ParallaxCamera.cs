d46aba2b2ee5785824a6bdf1a119c8f4f4318dc6
LUKAS Margaux <m.lukas@rubika-edu.com>
2019-05-03 01:01:22 +0200

    Scene [Puzzle01] - Rose Animation & Parallax Dream & Stairs Animation

---
+++ Assets/Scripts/ParallaxCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParallaxCamera : MonoBehaviour
+{
+    public delegate void ParallaxCameraDelegate(float deltaMovement);
+    public ParallaxCameraDelegate onCameraTranslate;
+
+    private float oldPositionX;
+    private float oldPositionY;
+
+	void Start ()
+    {
+        oldPositionX = transform.position.x;
+        oldPositionY = transform.position.y;
+	}
+
+	void Update ()
+    {
+		if (transform.position.x != oldPositionX)
+        {
+            if (onCameraTranslate != null)
+            {
+                float delta = oldPositionX - transform.position.x;
+                onCameraTranslate(delta);
+            }
+
+            oldPositionX = transform.position.x;
+        }
+
+        if (transform.position.y != oldPositionY)
+        {
+            if (onCameraTranslate != null)
+            {
+                float delta = oldPositionY - transform.position.y;
+                onCameraTranslate(delta);
+            }
+
+            oldPositionY = transform.position.y;
+        }
+    }
+}
