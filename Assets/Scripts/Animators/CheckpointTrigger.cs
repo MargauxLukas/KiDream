6f6254b55fb8e89991229eb068a96d500c590270
LUKAS Margaux <m.lukas@rubika-edu.com>
2019-05-05 00:42:53 +0200

    Scene [Puzzle01] - First Platform Polish (waiting for GAs...)

---
+++ Assets/Scripts/Animators/CheckpointTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointTrigger : MonoBehaviour
+{
+    Animator CheckpointAnim;
+    public bool isChecked = false;
+
+    void Start()
+    {
+        CheckpointAnim = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(CheckpointAnim);
+        if (collision.tag == "Player" && isChecked == false)
+        {
+            CheckpointAnim.SetBool("isChecked", true);
+            isChecked = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Animators/FixOrderInLayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixOrderInLayer : MonoBehaviour
+{
+    SpriteRenderer sprite;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<SpriteRenderer>() != null)
+        {
+            sprite = collision.GetComponent<SpriteRenderer>();
+            sprite.sortingOrder = 1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<SpriteRenderer>() != null)
+        {
+            sprite = collision.GetComponent<SpriteRenderer>();
+            sprite.sortingOrder = 0;
+        }
+    }
+}
