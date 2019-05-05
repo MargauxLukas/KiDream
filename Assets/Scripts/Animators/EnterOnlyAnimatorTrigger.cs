7f48a0184591095f2837f026fea92004bf2b9ab3
LUKAS Margaux <m.lukas@rubika-edu.com>
2019-05-05 02:05:11 +0200

    Scene [Puzzle01] - Tilemap Animation WIP

---
+++ Assets/Scripts/Animators/EnterOnlyAnimatorTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterOnlyAnimatorTrigger : MonoBehaviour
+{
+    Animator Anim;
+    public bool isChecked = false;
+
+    void Start()
+    {
+        Anim = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(Anim);
+        if (collision.tag == "Player" && isChecked == false)
+        {
+            Anim.SetBool("isTouched", true);
+            isChecked = true;
+        }
+    }
+}
