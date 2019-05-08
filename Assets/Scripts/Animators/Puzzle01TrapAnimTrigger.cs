dd7014668b50c04960067d2335c5c61ea46695ae
LUKAS Margaux <m.lukas@rubika-edu.com>
2019-05-09 01:57:36 +0200

    Scene [Puzzle01] - Tilemaps & PuzzleTuto WIP

---
+++ Assets/Scripts/Animators/Puzzle01TrapAnimTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle01TrapAnimTrigger : MonoBehaviour
+{
+    Animator Anim;
+    BoxCollider2D collider;
+    public GameObject Cube;
+    public bool isChecked = false;
+
+    void Start()
+    {
+        Anim = GetComponentInParent<Animator>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == Cube && isChecked == false)
+        {
+            Anim.SetBool("isTouched", true);
+            isChecked = true;
+            collider.isTrigger = false;
+            Destroy(this);
+        }
+    }
+}
