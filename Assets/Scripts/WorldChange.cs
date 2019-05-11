d57076bb5a79c4718232bc984e27ad4a884d5ed5
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-05-11 21:59:44 +0200

    Cube Prefab

---
+++ Assets/Scripts/WorldChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldChange : MonoBehaviour
+{
+    private GameObject player;
+    private Animator animator;
+    private bool isDream;
+
+    private void Start()
+    {
+        player   = GameObject.Find("Player");
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(player.GetComponent<CharacterController>().isDream)
+        {
+            animator.SetBool("isDream", true);
+        }
+        else
+        {
+            animator.SetBool("isDream", false);
+        }
+    }
+
+}
