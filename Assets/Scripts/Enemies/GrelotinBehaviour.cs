a718a5e5608732052ff7912a62550fb322397e88
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-05-11 15:23:27 +0200

    Grelotin 1.1 - Prefab

--- Assets/Scripts/Enemies/GrelotinBehaviour.cs
+++ Assets/Scripts/Enemies/GrelotinBehaviour.cs
@@ -4,26 +4,50 @@ using UnityEngine;
 
 public class GrelotinBehaviour : MonoBehaviour
 {
+    [Header("Objet = New Target (Sympa pour scripter)")]
+    public GameObject attachedObject; //Objet auquel il est attaché, il passera son temps à le pousser (Sympa pour scripté)
+    [Header("PushCorrupted en enfant")]
+    public GameObject pushCorrupted1;
+    public GameObject pushCorrupted2;
+    public GameObject pushCorrupted3;
+    public GameObject pushCorrupted4;
+
     private bool isDream     =  true;
     private bool isAttacking = false;
+    private bool canMove     = true;
+    [Header("PushCorrupted power")]
+    public float PushPower = 0.1f;
 
     private GameObject player;
     private Animator animator;
     private Transform  target;
-
+    [Header("Speed Movement")]
     public float speedMoving = 0.5f;
     private int lookingAt    =    0; // 1 = Droite, 2 = Down, 3 = Left, 4 = Up
 
     void Start ()
     {
+        pushCorrupted1.GetComponent<PushEffect>().forceX = PushPower;
+        pushCorrupted2.GetComponent<PushEffect>().forceX = PushPower;
+        pushCorrupted3.GetComponent<PushEffect>().forceX = PushPower;
+        pushCorrupted4.GetComponent<PushEffect>().forceX = PushPower;
         player = GameObject.Find("Player");
         animator = GetComponent<Animator>();
-        target = player.transform;
+
+        if (attachedObject != null)
+        {
+            target = attachedObject.transform;
+        }
+        else
+        {
+            target = player.transform;
+        }
     }
 	
 	void Update ()
     {
-        Move();
+        if (canMove){Move();}
+        else { animator.SetBool("isMoving", false); }
     }
 
     void Move()
@@ -70,18 +94,44 @@ public class GrelotinBehaviour : MonoBehaviour
         }
     }
 
-    void Attack()
+    public void Attack(Collider2D hit)
     {
-        isAttacking = true;
-        speedMoving = 1f;
-        animator.SetBool("isAttacking", true);
-        StartCoroutine(WaitAttack());
+        if (!isAttacking)
+        {
+            isAttacking = true;
+            animator.SetTrigger("isAttacking");
+            canMove = false;
+            StartCoroutine(WaitAttack());
+        }
+        //Repousse Objet
     }
 
     IEnumerator WaitAttack()
     {
+        if (lookingAt == 1) //Droite
+        {
+            pushCorrupted1.SetActive(true);
+        }
+        else if (lookingAt == 2) //Bas
+        {
+            pushCorrupted2.SetActive(true);
+        }
+        else if (lookingAt == 3) // Gauche
+        {
+            pushCorrupted3.SetActive(true);
+        }
+        else if (lookingAt == 4) //Haut
+        {
+            pushCorrupted4.SetActive(true);
+        }
+        yield return new WaitForSeconds(0.700f);
+        animator.SetTrigger("isSleep");
+        pushCorrupted1.SetActive(false);
+        pushCorrupted2.SetActive(false);
+        pushCorrupted3.SetActive(false);
+        pushCorrupted4.SetActive(false);
         yield return new WaitForSeconds(2f);
-        speedMoving = 0.5f;
+        canMove = true;
         isAttacking = false;
     }
 }
