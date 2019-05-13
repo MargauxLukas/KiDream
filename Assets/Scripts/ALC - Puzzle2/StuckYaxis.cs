7007e76d48fdad394cf261ff10c7bd591de6ca31
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-05-13 16:12:56 +0200

    Puzzle2 - Room 4 Done

--- Assets/Scripts/ALC - Puzzle2/StuckYaxis.cs
+++ Assets/Scripts/ALC - Puzzle2/StuckYaxis.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class StuckYaxis : MonoBehaviour
 {
+    public float minY;
+    public float maxY;
 
-	void Start ()
+    void Update()
     {
-		
-	}
-	
-	void Update ()
-    {
-		
-	}
+        if (transform.position.y < minY)
+        {
+            Debug.Log("INF");
+            transform.position = new Vector2(transform.position.x, minY);
+        }
+
+        if (transform.position.y > maxY)
+        {
+            Debug.Log("SUP");
+            transform.position = new Vector2(transform.position.x, maxY);
+        }
+    }
 }
