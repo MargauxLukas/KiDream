05217496d907456c1e3de5ec7d65e4c908674e18
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-05-14 23:10:23 +0200

    Puzzle 5

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //gameMaster = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+        if (collision.name == "Player")
+        {
+            Debug.Log("Player est tombé");
+            //Animation de mort
+            //Respawn
+        }
+        else if (collision.tag.Contains("Chick"))
+        {
+            Debug.Log("chick est tombé");
+            //Animation de mort
+        }
+        else if (collision.name.Contains("Pillar"))
+        {
+            Debug.Log("Pillier est tombé");
+        }
+    }
+}
