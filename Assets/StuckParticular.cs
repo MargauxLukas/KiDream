f14a56a22b20401e2d152a554f20ce4a3599dcb3
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-05-16 00:49:08 +0200

    Orgue Puzzle - Adapt Puzzle

---
+++ Assets/StuckParticular.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuckParticular : MonoBehaviour
+{
+    public float yPallier;
+    public float xPallier;
+
+    private void Update()
+    {
+        if(transform.localPosition.y > yPallier )
+        {
+            gameObject.GetComponent<StuckXaxis>().enabled = false;
+        }
+        else
+        {
+            gameObject.GetComponent<StuckXaxis>().enabled = true;
+        }
+
+        if (transform.localPosition.x < xPallier)
+        {
+            gameObject.GetComponent<StuckYaxis>().enabled = true;
+        }
+        else
+        {
+            //gameObject.GetComponent<StuckYaxis>().enabled = false;
+        }
+    }
+}
