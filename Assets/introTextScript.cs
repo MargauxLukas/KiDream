b43461052c37db9c79ab341bac13b737ec222184
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-05-21 22:26:35 +0200

    Intro scene 1.0 - Done

---
+++ Assets/introTextScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class introTextScript : MonoBehaviour
+{
+    public Text test;
+
+
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
