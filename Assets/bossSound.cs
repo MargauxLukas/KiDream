932d257963db47fc4537ab1f7f3ae782f65620c5
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-05-23 00:38:50 +0200

    Boss Intro Up grelot sound

--- Assets/bossSound.cs
+++ Assets/bossSound.cs
@@ -5,17 +5,59 @@ using UnityEngine.Audio;
 
 public class bossSound : MonoBehaviour
 {
-    public AudioClip grelotSound;
+    public AudioClip grelot1Sound;
+    public AudioClip grelot2Sound;
+    public AudioClip grelot3Sound;
+    public AudioClip grelot4Sound;
+    public AudioClip grelot5Sound;
+    public AudioClip grelot6Sound;
+    public AudioClip grelot7Sound;
     public AudioClip introHell;
     public AudioClip surpriseS;
 
     public AudioSource audioS;
     public AudioSource surpriseAS;
 
-    public void GrelotSound()
+    public void Grelot1Sound()
     {
         audioS.volume = 0.3f;
-        audioS.PlayOneShot(grelotSound);
+        audioS.PlayOneShot(grelot1Sound);
+    }
+
+    public void Grelot2Sound()
+    {
+        audioS.volume = 0.3f;
+        audioS.PlayOneShot(grelot2Sound);
+    }
+
+    public void Grelot3Sound()
+    {
+        audioS.volume = 0.3f;
+        audioS.PlayOneShot(grelot3Sound);
+    }
+
+    public void Grelot4Sound()
+    {
+        audioS.volume = 0.3f;
+        audioS.PlayOneShot(grelot4Sound);
+    }
+
+    public void Grelot5Sound()
+    {
+        audioS.volume = 0.3f;
+        audioS.PlayOneShot(grelot5Sound);
+    }
+
+    public void Grelot6Sound()
+    {
+        audioS.volume = 0.3f;
+        audioS.PlayOneShot(grelot6Sound);
+    }
+
+    public void Grelot7Sound()
+    {
+        audioS.volume = 0.3f;
+        audioS.PlayOneShot(grelot7Sound);
     }
 
     public void HellSound()
