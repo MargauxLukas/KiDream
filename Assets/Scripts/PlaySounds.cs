3c2ea4f6296dc707b617c6e4cb6b6eaa9bb34b55
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-05-22 15:26:29 +0200

    FIRST AUDIO !

---
+++ Assets/Scripts/PlaySounds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlaySounds : MonoBehaviour
+{
+    public AudioClip fallingWall;
+
+    public AudioSource audioS;
+
+    public void fallingWallSound()
+    {
+        audioS = gameObject.GetComponent<AudioSource>();
+        //audioS.PlayOneShot(fallingWall);
+        audioS.clip = fallingWall;
+        audioS.Play();
+    }
+}
