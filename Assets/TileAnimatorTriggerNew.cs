c59cb3d1681c82066f82a78956b31db1eef497da
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-05-25 15:05:43 +0200

    Prefab Partition

---
+++ Assets/TileAnimatorTriggerNew.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileAnimatorTriggerNew : MonoBehaviour
+{
+    private Animator anim;
+    BoxCollider2D Collider;
+    public bool isChecked = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        Collider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && isChecked == false)
+        {
+            anim.SetBool("isTouched", true);
+            isChecked = true;
+            Destroy(Collider);
+            Destroy(this);
+        }
+    }
+}
