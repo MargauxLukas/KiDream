cfeff295becb901a9b94db771f2b717762c42046
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-05-27 16:17:41 +0200

    Add portal open sound

---
+++ Assets/PortalSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalSound : MonoBehaviour
+{
+
+    public AudioClip portailOpen;
+
+    private AudioSource audioS;
+
+    public void portalOpenSound()
+    {
+        audioS = gameObject.GetComponent<AudioSource>();
+        audioS.PlayOneShot(portailOpen);
+    }
+}
